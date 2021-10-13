26d23d022ac744f77a190db629eb8151cc98361d
Vipin Mallaya <vkgopinathan@live.com>
2021-10-13 17:10:29 +0530

    Add project files.

---
+++ Controllers/CallbackController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+
+namespace QukBukSampleWeb.Controllers
+{
+    public class CallbackController : Controller
+    {
+        // GET: CallbackControllercs
+        public ActionResult Index()
+        {
+            var value = HttpContext.Request.QueryString;
+            //string code = HttpContext.Request.QueryString["code"] ?? "none";
+            //string realmId = Request.QueryString["realmId"] ?? "none";
+            return View();
+        }
+
+        // GET: CallbackControllercs/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CallbackControllercs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CallbackControllercs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CallbackControllercs/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CallbackControllercs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CallbackControllercs/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CallbackControllercs/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Intuit.Ipp.OAuth2PlatformClient;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace QukBukSampleWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Instantiate OAuth2Client object with clientId, clientsecret, redirectUrl and environment
+        public static OAuth2Client auth2Client;
+        public string Index()
+        {
+            return "hello First Time";
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        //Generate authorize url to get the OAuth2 code
+        public ActionResult InitiateAuth()
+        {
+            var clientid = "ABTxW6lPn1V4YMmvANavfol94a5iuTBDTeg2649dUIAr7OPErt"; //ConfigurationManager.AppSettings["clientid"] as string;
+            var clientsecret = "irYUpW7oCl7QosLhQjmk4u3pNjNR9jujN0XBLlBq"; // ConfigurationManager.AppSettings["clientsecret"] as string;
+            var redirectUrl = "https://localhost:44300/home/callback"; //ConfigurationManager.AppSettings["redirectUrl"] as string; 
+            var environment = "sandbox";//ConfigurationManager.AppSettings["appEnvironment"] as string; ;
+
+            auth2Client ??= new OAuth2Client(clientid, clientsecret, redirectUrl, environment);
+
+            List<OidcScopes> scopes = new List<OidcScopes>();
+            scopes.Add(OidcScopes.Accounting);
+            string authorizeUrl = auth2Client.GetAuthorizationURL(scopes);
+            return Redirect(authorizeUrl);
+        }
+
+        public string Callback()
+        {
+            var value = HttpContext.Request.QueryString;
+            return "This is the Welcome action method...";
+        }
+    }
+}
