55a5188d5d3fd8fa80792350114942ad2e0f9c83
Vipin Mallaya <vkgopinathan@live.com>
2021-10-14 11:24:17 +0530

    Updated Controller and Methods Also added HTTP Request

--- Controllers/CallbackController.cs
+++ Controllers/CallbackController.cs
@@ -2,12 +2,16 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace QukBukSampleWeb.Controllers
 {
     public class CallbackController : Controller
     {
+        private object _accessToken;
+        private object _RefreshToken;
+
         // GET: CallbackControllercs
         public ActionResult Index()
         {
@@ -15,7 +19,19 @@ namespace QukBukSampleWeb.Controllers
             var result = HttpUtility.ParseQueryString(value);
 
             string code = result["code"] ?? "none";
-            string realmId = result["realmId"] ?? "none";
+           // string realmId = result["realmId"] ?? "none";
+
+
+            // Get OAuth2 Bearer token
+            HomeController.auth2Client.GetBearerTokenAsync(code).ContinueWith(async (response) =>
+               {
+                   var tokenResult = await response;
+                   _accessToken = tokenResult.AccessToken;
+                   _RefreshToken = tokenResult.RefreshToken;
+               });
+
+            //retrieve access_token and refresh_token
+
 
             return View();
         }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Web;
 
 namespace QukBukSampleWeb.Controllers
 {
@@ -9,6 +10,9 @@ namespace QukBukSampleWeb.Controllers
     {
         //Instantiate OAuth2Client object with clientId, clientsecret, redirectUrl and environment
         public static OAuth2Client auth2Client;
+        private string _AccessToken;
+        private string _RefreshToken;
+
         public string Index()
         {
             return "hello First Time";
@@ -23,11 +27,9 @@ namespace QukBukSampleWeb.Controllers
         {
             var clientid = "ABTxW6lPn1V4YMmvANavfol94a5iuTBDTeg2649dUIAr7OPErt"; //ConfigurationManager.AppSettings["clientid"] as string;
             var clientsecret = "irYUpW7oCl7QosLhQjmk4u3pNjNR9jujN0XBLlBq"; // ConfigurationManager.AppSettings["clientsecret"] as string;
-            var redirectUrl = "https://localhost:44300/callback"; //ConfigurationManager.AppSettings["redirectUrl"] as string; 
+            var redirectUrl = "https://localhost:44300/home/callback"; //ConfigurationManager.AppSettings["redirectUrl"] as string; 
             var environment = "sandbox";//ConfigurationManager.AppSettings["appEnvironment"] as string; ;
 
-            
-            
             auth2Client ??= new OAuth2Client(clientid, clientsecret, redirectUrl, environment);
 
             List<OidcScopes> scopes = new List<OidcScopes>();
@@ -38,8 +40,23 @@ namespace QukBukSampleWeb.Controllers
 
         public string Callback()
         {
-            var value = HttpContext.Request.QueryString;
-            return "This is the Welcome action method...";
+            var value = HttpContext.Request.QueryString.Value;
+            var result = HttpUtility.ParseQueryString(value);
+
+            string code = result["code"] ?? "none";
+            string realmId = result["realmId"] ?? "none";
+
+            // Get OAuth2 Bearer token
+            auth2Client.GetBearerTokenAsync(code).ContinueWith(async (response) =>
+            {
+                var tokenResult = await response;
+                _AccessToken = tokenResult.AccessToken;
+                _RefreshToken = tokenResult.RefreshToken;
+                
+            });
+
+            //retrieve access_token and refresh_token
+            return $"This is the Welcome action method...Access Token -{_AccessToken} Refresh Token - {_RefreshToken}";
         }
     }
 }
